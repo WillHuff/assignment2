ff9a6b191191869c9914c72f0bd275fe9d79f080
William Huff <huff.sterling@gmail.com>
2019-10-25 18:10:40 -0500

    Initial commit

---
+++ src/mainy.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.Scanner;
+public class mainy {
+    public static void main(String[] args) throws IOException {
+            System.out.println("Enter the number of students to be entered.");
+            Scanner darkly = new Scanner(System.in);//System scanner
+            int n = darkly.nextInt();//Option selector
+        int data[]= new int[n];
+        for(int i=0;i<n;i++) {
+            System.out.println("Enter the number of students to be entered.");
+            data[0]=darkly.nextInt();
+        }
+    }
+    public static void (){
+
+    }
+}
+
